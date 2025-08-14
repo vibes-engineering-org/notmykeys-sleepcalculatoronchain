7e0719df1844e10a67ecda95a89b1123d456a9de
vibesengineering <205912982+vibesengineering@users.noreply.github.com>
2025-08-14 05:55:54 +0000

    Initial commit from template

---
+++ src/lib/constants.ts
@@ -0,0 +1,5 @@
+export const PROJECT_TITLE = "SleepCalculatorOnchain";
+export const PROJECT_DESCRIPTION = "by @notmykeys";
+export const PROJECT_CREATOR = "notmykeys";
+export const PROJECT_AVATAR_URL =
+  "https://wrpcd.net/cdn-cgi/imagedelivery/BXluQx4ige9GuW0Ia56BHw/37460dae-996e-481a-9c2c-0374da719400/anim=false,fit=contain,f=auto,w=576";
